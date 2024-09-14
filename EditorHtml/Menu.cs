da934920d94ac83b9f922b2a2d79afc89f4ae328
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-14 18:57:34 -0300

    Editor HTML
    Menu

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,44 @@
+namespace EditorHtml {
+    public static class Menu{
+        public static void Show(){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+            WriteOptions();
+        }
+
+        public static void DrawScreen(){
+            Console.Write("+");
+            for(int i = 0; i<30; i++){
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for(int linha = 0; linha <= 10; linha++){
+                Console.Write("|");
+
+                for(int i = 0; i<30; i++){
+                    Console.Write(" ");
+            }
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+            
+                Console.Write("+");
+                for(int i = 0; i<30; i++){
+                    Console.Write("-");
+                }
+                Console.Write("+");
+                Console.Write("\n");
+        }
+
+        public static void WriteOptions(){
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+        }
+    }
+}
