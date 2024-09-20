14d1542d6059f179e0cd9760947784bbca304bed
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-20 00:21:21 -0300

    aula final
    Falta salvar o arquivo

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -7,6 +7,9 @@ namespace EditorHtml {
 
             DrawScreen();
             WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen(){
@@ -39,6 +42,34 @@ namespace EditorHtml {
         public static void WriteOptions(){
             Console.SetCursorPosition(3,2);
             Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("===========");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir Arquivo");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opções: ");
+
+
+
+        }
+
+        public static void HandleMenuOption(short option){
+            switch(option){
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }
---
+++ EditorHtml/Viewer.cs
@@ -0,0 +1,41 @@
+using System.Text.RegularExpressions;
+
+namespace EditorHtml{
+    public class Viewew {
+        public static void Show(String text){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("-------------");
+            Replace(text);
+            Console.WriteLine("-------------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text){
+            var strong = new Regex(@"<\s*string[^>]*>(.*?)<\$*/\s*strong>");
+            var words = text.Split(' ');
+
+            for(var i = 0; i< words.Length; i++){
+                if(strong.IsMatch(words[i])){
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            ((words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>'))
+                        )
+
+                    );
+                    Console.Write(" ");
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
